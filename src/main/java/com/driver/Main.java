60be2b19bd6f8fb9e4f8b885571e1be8fadf689c
Abhishek  kandari <abhishekkandari96@gmail.com>
2023-05-13 12:43:13 +0530

    solved assigment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
+    public static void main(String args[]){
+        RWOnly obj=new RWOnly();
+//        obj.name="Abhishek";
+//        System.out.println(obj.name);
+        // Error: java: name has private access in com.driver.RWOnly
+        obj.setName("Abhishek");
+        String name=obj.getName();
+        System.out.println(name);
+
+    }
   
 }
